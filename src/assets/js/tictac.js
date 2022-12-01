59ac61f7edfe86bbf46364dbfaa328e677c90d6c
vrangr1 <anavprasadanav@gmail.com>
2022-12-01 02:05:35 -0500

    prime squares game tic tac toe version working

--- src/assets/js/tictac.js
+++ src/assets/js/tictac.js
@@ -5,17 +5,17 @@ player_score = 0;
 computer_score = 0;
 counter = 0;
 last_move_index = -1;
-
+primes_showing = false;
 
 board_full = false;
 winner_found = false;
-// play_board = ["", "", "", "", "", "", "", "", ""];
 play_board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 occupied = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 html_board = document.getElementsByClassName("block");
 winner_statement = document.getElementById("winner");
 html_player_score = document.getElementById("player-score");
 html_computer_score = document.getElementById("computer-score");
+html_show_primes = document.getElementById("show_primes");
 
 initial_text_player_score = html_player_score.innerText;
 initial_text_computer_score = html_computer_score.innerText;
@@ -55,10 +55,10 @@ check_board_complete = () => {
 // };
 
 check_if_prime = num => {
-    for (i = 2; i <= Math.sqrt(num); ++i){
-        if (num % i == 0) return true;
+    for (j = 2; j <= Math.sqrt(num); ++j){
+        if (num % j == 0) return false;
     }
-    return false;
+    return true;
 };
 
 check_line = (a, b, c) => {
@@ -103,19 +103,30 @@ check_match = () => {
   return "";
 };
 
+winner_found_now = () => {
+    play_board.forEach((element, index) => {
+        if (occupied[index] == 0)
+            html_board[index].className += " " + "occupied";
+        occupied[index] = 3;
+      });
+}
+
 check_for_winner = () => {
   winner_found = check_match();
   if (winner_found == player) {
     player_score += 1;
     winner.innerText = "Winner is Player One!!";
     winner.classList.add("playerWin");
+    winner_found_now();
   } else if (winner_found == playerTwo) {
     computer_score += 1;
     winner.innerText = "Winner is Player Two";
     winner.classList.add("computerWin");
+    winner_found_now();
   } else if (board_full) {
     winner.innerText = "Draw!";
     winner.classList.add("draw");
+    winner_found_now();
   }
   html_player_score.innerText = initial_text_player_score + " " + player_score;
   html_computer_score.innerText =
@@ -123,53 +134,29 @@ check_for_winner = () => {
 };
 
 addPlayerMove = e => {
-  if (!winner_found && occupied[e] == 0) {
+  if (winner_found == 0 && occupied[e] == 0) {
     if (last_move_index != -1 && last_move_index != e){
         occupied[last_move_index] = (counter % 2) + 1;
         last_move_index = e;
+        counter++;
     }
-    play_board[e] = (play_board[e] + 1)%9;
-    counter++;
+    play_board[e] = (play_board[e] + 1) % 10;
+    last_move_index = e;
     render_board();
-    // check_board_complete();
     check_for_winner();
+    // print_stuff();
     return;
-    play_board[e] = (counter % 2 == 0 ? player : playerTwo);
-    // if (!board_full) {
-    //   addComputerMove();
-    // }
   } else {
   }
 };
 
-// addPlayerTwoMove = e => {
-//     if (!winner_found && play_board[e] == "") {
-//       play_board[e] = playerTwo;
-//       render_board();
-//       check_board_complete();
-//       check_for_winner();
-//     //   if (!board_full) {
-//     //     addComputerMove();
-//     //   }
-//     } else {
-//     }
-//   };
-
-// addComputerMove = () => {
-//   if (!winner_found) {
-//     do {
-//       selected = Math.floor(Math.random() * 9);
-//     } while (play_board[selected] != "");
-//     play_board[selected] = computer;
-//     render_board();
-//     check_for_winner();
-//   }
-// };
-
 reset_board = () => {
-  play_board = ["", "", "", "", "", "", "", "", ""];
+  play_board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  occupied = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   board_full = false;
-  winner_found = false;
+  winner_found = 0;
+  counter = 0;
+  last_move_index = -1;
   play_board.forEach((element, index) => {
     html_board[index].classList.remove("occupied");
     html_board[index].classList.remove("win");
@@ -187,4 +174,42 @@ reset_match = () => {
   html_player_score.innerText = initial_text_player_score + " " + player_score;
   html_computer_score.innerText =
     initial_text_computer_score + " " + computer_score;
+};
+
+end_game = () => {
+    ++counter;
+    occupied[last_move_index] = (counter % 2) + 1;
+    html_board[last_move_index].className += " " + "occupied";
+    board_full = true;
+    check_for_winner();
+
+    // reset_board();
+};
+
+print_stuff = () => {
+    // html_board[9].innerText = occupied[last_move_index].toString() + " this";
+    out_string = "; "
+    for (k = 0; k < 8; ++k) out_string = out_string + (occupied[k].toString() + ",")
+    out_string += occupied[8].toString()
+    debug_print.innerText = "last:" + last_move_index.toString() + out_string
+    // document.getElementById("demo").innerHTML = occupied[last_move_index].toString() + " this";;
+};
+
+toggle_primes = () => {
+    primes_showing = !primes_showing;
+    if (primes_showing) show_all_primes();
+    else show_primes.innerText = "";
+}
+
+show_all_primes = () => {
+    show_primes.innerText = "Here is a list of primes between 100 and 999:\n101 103\n\
+    107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211\n\
+    223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331\n\
+    337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449\n\
+    457 461 463 467 479 487 491 499 503 509 521 523 541 547 557 563 569 571 577 587\n\
+    593 599 601 607 613 617 619 631 641 643 647 653 659 661 673 677 683 691 701 709\n\
+    719 727 733 739 743 751 757 761 769 773 787 797 809 811 821 823 827 829 839 853\n\
+    857 859 863 877 881 883 887 907 911 919 929 937 941 947 953 967 971 977 983 991\n\
+    997\n\
+    ";
 };
