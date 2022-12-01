dd61b7bea6a31e4c0d7d3e381524fab8cb68c37d
vrangr1 <anavprasadanav@gmail.com>
2022-12-01 05:22:17 -0500

    Prime Squares Game implemented; few edge cases to fix

--- src/assets/js/tictac.js
+++ src/assets/js/tictac.js
@@ -17,6 +17,7 @@ html_player_score = document.getElementById("player_score");
 html_player_two_score = document.getElementById("player_two_score");
 html_show_primes = document.getElementById("show_primes");
 html_player_turn = document.getElementById("player_turn");
+const found_primes = new Set();
 
 initial_text_player_score = player_score.innerText;
 initial_text_player_two_score = player_two_score.innerText;
@@ -59,6 +60,7 @@ reset_board = () => {
     play_board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     occupied = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     board_full = false;
+    found_primes.clear();
     winner_found = 0;
     counter = 0;
     last_move_index = -1;
@@ -86,12 +88,16 @@ check_if_prime = num => {
     return 1;
 };
 
+create_number = (num1, num2, num3) => {
+    return (num1 * 100 + num2 * 10 + num3);
+}
+
 check_line = (a, b, c) => {
     if (occupied[a] == 0 || occupied[b] == 0 || occupied[c] == 0) return 0;
     return (
-        check_if_prime(play_board[a]* 100 + play_board[b] * 10 + play_board[c])
+        check_if_prime(create_number(play_board[a], play_board[b], play_board[c]))
         +
-        check_if_prime(play_board[c]* 100 + play_board[b] * 10 + play_board[a])
+        check_if_prime(create_number(play_board[c], play_board[b], play_board[a]))
     );
 };
 
@@ -99,35 +105,38 @@ check_match = () => {
     let prime_count = 0, current_count = 0;
     for (i = 0; i < 9; i += 3) {
         current_count = check_line(i, i + 1, i + 2);
-        prime_count += current_count;
-        if (current_count > 0) {
+        if (current_count > 0 && !found_primes.has(create_number(i, i+1, i+2))) {
             html_board[i].classList.add("win");
             html_board[i + 1].classList.add("win");
             html_board[i + 2].classList.add("win");
+            found_primes.add(create_number(i, i+1, i+2));
+            prime_count += current_count;
         }
     }
     for (i = 0; i < 3; i++) {
         current_count = check_line(i, i + 3, i + 6);
-        prime_count += current_count;
-        if (current_count > 0) {
+        if (current_count > 0 && !found_primes.has(create_number(i, i+3, i+6))) {
             html_board[i].classList.add("win");
             html_board[i + 3].classList.add("win");
             html_board[i + 6].classList.add("win");
+            found_primes.add(create_number(i, i+3, i+6));
             prime_count += current_count;
         }
     }
     current_count = check_line(0, 4, 8);
-    if (current_count > 0) {
+    if (current_count > 0 && !found_primes.has(48)) {
         html_board[0].classList.add("win");
         html_board[4].classList.add("win");
         html_board[8].classList.add("win");
+        found_primes.add(48);
         prime_count += current_count;
     }
     current_count = check_line(2, 4, 6);
-    if (current_count > 0) {
+    if (current_count > 0 && !found_primes.has(246)) {
         html_board[2].classList.add("win");
         html_board[4].classList.add("win");
         html_board[6].classList.add("win");
+        found_primes.add(create_number(246));
         prime_count += current_count;
     }
     return prime_count;
